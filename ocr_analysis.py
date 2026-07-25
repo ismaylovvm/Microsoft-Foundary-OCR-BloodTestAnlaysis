206eef903c8b7eb508184c323b17e4fec71cf186
Mahammad <ismayilzade.mhmmdd@gmail.com>
2026-07-25 16:27:58 +0300

    Belge analizi için OCR modeli eklendi

---
+++ ocr_analysis.py
@@ -0,0 +1,33 @@
+
+
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.documentintelligence import DocumentIntelligenceClient
+from azure.ai.documentintelligence.models import AnalyzeDocumentRequest
+from  process_analysis import parse_table
+
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+endpoint = os.getenv("ocr_endpoint")
+key = os.getenv("ocr_key")
+
+# sample document
+
+document_intelligence_client  = DocumentIntelligenceClient(
+    endpoint=endpoint, credential=AzureKeyCredential(key)
+)
+
+def analyze_table(image):
+    with open(image, "rb") as f:
+        poller = document_intelligence_client.begin_analyze_document(
+            "prebuilt-layout",
+            body=f,
+            content_type="application/octet-stream"
+        )
+    result = poller.result()
+
+    return result
